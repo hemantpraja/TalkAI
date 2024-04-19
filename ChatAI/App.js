a2b0bca5cce9399b1d8eba6f47abe795dceaa893
Hemant Prajapati <hemantpraja88@gmail.com>
2024-04-19 11:20:36 +0530

    first commit

---
+++ ChatAI/App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+// import App from './src/navigation'
+import AppNavigation from './src/navigation'
+
+const App = () => {
+  return (<>
+    <AppNavigation />
+  </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
